2fee080fa13106ee479488c4b6823c26db365add
Jayden Olsen <Jayden@Jaydens-Mac-mini.local>
2017-04-03 01:14:41 -0400

    TouchTracker after chapter 18, no challenges completed

---
+++ touchTracker/Line.swift
@@ -0,0 +1,14 @@
+//
+//  Line.swift
+//  touchTracker
+//
+//  Created by Jayden Olsen on 4/3/17.
+//  Copyright © 2017 Jayden Olsen. All rights reserved.
+//
+
+import CoreGraphics
+
+struct Line {
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+}
